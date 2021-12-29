39eb0cb36012d9085aa31009b699b7add3ca74ed
Ward Jubran <you@example.com>
2021-12-29 14:53:12 +0200

    Homework9 included

--- Homework 8/app.py
+++ Homework 8/app.py
@@ -1,7 +1,7 @@
-from flask import Flask, redirect, url_for, render_template
+from flask import Flask, redirect, url_for, render_template, request, session
 
-app = Flask (__name__)
-
+app = Flask(__name__)
+app.secret_key = '123'
 
 # Creating the default route "/" and connecting it to the CV HTML page that i created in previous homeworks: (added
 # it to templates)
@@ -31,10 +31,83 @@ def contactme_func():
 
 @app.route("/assignment8")
 def assignment8_func():
-    return render_template('assignment8.html', tvshows=('Mr. Robot', 'Vikings', 'Peaky Blinders', 'Rick And Morty', 'Sherlock'))
+    return render_template('assignment8.html',
+                           tvshows=('Mr. Robot', 'Vikings', 'Peaky Blinders', 'Rick And Morty', 'Sherlock'))
+
 
 # Now that I created the assignment 8 html, I will continue my work on the html file assignment8.html
 
 
+# ********************************************* Assignment 9 ****************************************************
+
+# Created an HTML template called Assignment 9.
+
+
+# Creating Assignment 9 route:
+@app.route('/assignment9', methods=['GET', 'POST'])
+def assignment9_func():
+    if request.method == 'GET':           # if we enter with get method
+        if 'name' in request.args:
+            name = request.args['name']   # get the name from the form input
+            session['name'] = name
+            if name == '':                # if the user did an empty search
+                return render_template('assignment9.html',
+                                       users={'user1': {'name': 'ward', 'email': 'ward@post.com'},
+                                              'user2': {'name': 'alex', 'email': 'Alex@gmail.com'},
+                                              'user3': {'name': 'yossi', 'email': 'Yossi@yo.com'},
+                                              'user4': {'name': 'jimmy', 'email': 'JimJim@jimmy.com'},
+                                              'user5': {'name': 'moshe', 'email': 'moshimosh@moshi.co.il'}})
+
+            else:      # if the user searched for a certain name
+                return render_template('assignment9.html', name=name,
+                                       users={'user1': {'name': 'ward', 'email': 'ward@post.com'},
+                                              'user2': {'name': 'alex', 'email': 'Alex@gmail.com'},
+                                              'user3': {'name': 'yossi', 'email': 'Yossi@yo.com'},
+                                              'user4': {'name': 'jimmy', 'email': 'JimJim@jimmy.com'},
+                                              'user5': {'name': 'moshe', 'email': 'moshimosh@moshi.co.il'}})
+
+        else:         # if the user didn't do anything (just entered the page):
+            return render_template('assignment9.html')
+
+    if request.method == 'POST':        # if the user used the Post form (registration)
+        name = request.form['nickname']  ##form is for form post parameters, args is for GET method parameters
+        password = request.form['password']
+        # TODO DB CHECK IF USER INFO EXISTS
+        session['username'] = name
+        return render_template('assignment9.html')
+    return render_template('assignment9.html')
+
+
+@app.route('/logout')  # create logout
+def logout_func():
+    session['username'] = ''
+    return redirect(url_for('assignment9_func'))
+
+
+
+
+
+
+
+
+
+# @app.route("/assignment9", methods=['GET', 'POST'])
+# def assignment9_func():
+#     if request.method == 'GET':
+#         if 'name' in request.args:  # if the user entered a name when submitting
+#             session['name'] = 'name'
+#         return render_template('assignment9.html', users=users)
+#     return render_template('assignment9.html')
+
+    #         userdict = users
+    #         for key, value in userdict:
+    #             if value.name == 'name':
+    #                 name = value.name
+    #                 email = value.email
+    #         return render_template('assignment9.html', name=name, email=email)
+    #     return render_template('assignment9.html', users=users)
+    # return render_template('assignment9.html')
+
+
 if __name__ == '__main__':
-    app.run(debug=True)     #this is how we intialize the server.
+    app.run(debug=True)  # this is how we intialize the server.
